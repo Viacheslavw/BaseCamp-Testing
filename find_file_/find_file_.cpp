6a9d4cbde41a4a929e01f80cdad543dbe9359e41
Viacheslav <77392805+Viacheslavw@users.noreply.github.com>
2022-06-09 16:25:55 +0300

    My test efforts
    My test efforts

---
+++ find_file_/find_file_.cpp
@@ -0,0 +1,78 @@
+﻿
+#include "find_file_.h"
+#include <thread>
+
+Function_pool::Function_pool() : m_function_queue(), m_lock(), m_data_condition(), m_accept_functions(true)
+{
+}
+
+Function_pool::~Function_pool()
+{
+}
+
+void Function_pool::push(std::function<void()> func)
+{
+    std::unique_lock<std::mutex> lock(m_lock);
+    m_function_queue.push(func);
+    lock.unlock();
+    m_data_condition.notify_one();
+}
+
+void Function_pool::done()
+{
+    std::unique_lock<std::mutex> lock(m_lock);
+    m_accept_functions = false;
+    lock.unlock();
+    m_data_condition.notify_all();
+
+}
+
+void Function_pool::infinite_loop_func()
+{
+    std::function<void()> func;
+    while (true)
+    {
+        {
+            std::unique_lock<std::mutex> lock(m_lock);
+            m_data_condition.wait(lock, [this]() {return !m_function_queue.empty() || !m_accept_functions; });
+            if (!m_accept_functions && m_function_queue.empty())
+            {
+
+                return;
+            }
+            func = m_function_queue.front();
+            m_function_queue.pop();
+        }
+        func();
+    }
+}
+Function_pool func_pool;
+
+class quit_worker_exception : public std::exception {};
+string name_file;
+void thead_function()
+{
+    find_file(name_file);
+}
+
+int main(int argc, char* argv[])
+{
+ 
+
+    std::cout << "stating operation" << std::endl;
+    int num_threads = 2;
+    std::cout << "number of threads = " << num_threads << std::endl;
+    std::vector<std::thread> thread_pool;
+    for (int i = 0; i < num_threads; i++)
+    {
+        thread_pool.push_back(std::thread(&Function_pool::infinite_loop_func, &func_pool));
+    }
+        cout << "Print name of file  ";
+        cin >> name_file;
+        func_pool.push(thead_function);
+    func_pool.done();
+    for (unsigned int i = 0; i < thread_pool.size(); i++)
+    {
+        thread_pool.at(i).join();
+    }
+}
---
+++ find_file_/find_file_.h
@@ -0,0 +1,96 @@
+﻿
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string>
+#include <time.h>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <cassert>
+
+
+using namespace std;
+
+class Function_pool
+{
+
+private:
+    std::queue<std::function<void()>> m_function_queue;
+    std::mutex m_lock;
+    std::condition_variable m_data_condition;
+    std::atomic<bool> m_accept_functions;
+
+public:
+
+    Function_pool();
+    ~Function_pool();
+    void push(std::function<void()> func);
+    void done();
+    void infinite_loop_func();
+};
+
+
+WIN32_FIND_DATA findfile,f;
+HANDLE hfile;
+LPCSTR file = "C:\\*";
+string name = "s.txt";
+string path = "C:\\", path2 = "*";
+
+
+void logic(HANDLE rhfile) {
+    
+
+    while (FindNextFile(rhfile, &findfile))
+    {
+        Sleep(10);
+        wcout << " " << findfile.cFileName << endl;
+       
+        path2 = findfile.cFileName;
+        if (findfile.cFileName == name) {
+            cout << "Find your file ";
+            cout << path;
+            wcout << findfile.cFileName << endl;
+            return;
+        }
+    }
+    
+    path = "C:\\";
+    path = path + path2;
+    path = path + "\\*";
+    cout << path << endl;
+    rhfile = FindFirstFile((path + "*.*").c_str(), &findfile);
+
+    while (FindNextFile(rhfile, &findfile))
+    {
+        Sleep(10);
+        wcout << "-  " << findfile.cFileName << endl;
+
+        path2 = findfile.cFileName;
+  
+        logic(rhfile);
+
+    }
+        
+}
+void find_file(string name1) {
+    name = name1;
+    hfile = FindFirstFile( file,&findfile);
+
+    string path, path2;
+    if (INVALID_HANDLE_VALUE == hfile)
+    {
+        cout << "error" << GetLastError() << endl;
+        return ;
+    }
+    else {
+          logic(hfile);
+        }
+    
+    FindClose(hfile);
+
+}
